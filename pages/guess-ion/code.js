1b566b0e58e0e7fa1859488c4b5d574c94c5adf7
João Lourenço <estudante.lourenco@gmail.com>
2022-02-22 18:59:52 +0000

    Add basic level selector and game layout

--- pages/guess-ion/code.js
+++ pages/guess-ion/code.js
@@ -6,8 +6,9 @@ function closeInstructions() {
     var instructions = document.getElementById("start");
     instructions.classList.add("close-rule-card");
     setTimeout(() => {
-        instructions.style.display = "none";
         instructions.classList.remove("close-rule-card");
+        var instructions_container = document.getElementById("start-game-container");
+        instructions_container.style.display = "none";
         var game = document.getElementById("game-container");
         game.style.display = "block";
 
@@ -20,4 +21,17 @@ function openInstructions() {
     instructions.style.display = "block";
     var game = document.getElementById("game-container");
     game.style.display = "none";
+}
+
+function startLevel(level) {
+    console.log("Level " + level + " started.");
+
+    if (level == 1) {
+        // Only one square should exist
+        document.getElementById("ion2").style.display = "none";
+        document.getElementById("ion3").style.display = "none";
+    }
+
+    document.getElementById("level-selector").style.display = "none";
+    document.getElementById("game").style.display = "flex";
 }
